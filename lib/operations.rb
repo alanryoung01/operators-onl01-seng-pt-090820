4480bf5ae3f22f45007a2d7eed746cf8ab15f927
Alan Young <alanryoung01@gmail.com>
2020-09-10 20:47:54 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,8 +11,8 @@ end
 
 
 def not_safe?(speed)
-if speed < 40 ? return true : return false
-elsif speed > 60 ? return true : return false
-elsif speed > 40 && speed < 60 ? return false : return true
+if speed < 40 ? true : false
+elsif speed > 60 ? true : false
+elsif speed > 40 && speed < 60 ? false : true
 end
 end
