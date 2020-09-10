80b6d4faf13073419f35420240deb871619aaf1a
Alan Young <alanryoung01@gmail.com>
2020-09-10 21:04:14 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -9,9 +9,9 @@ else
 end
 end
 
-def not_safe?(speed)
-if speed < 40 ? true : false
-elsif speed > 60 ? true : false
-elsif speed > 40 && speed < 60 ? false : true
+def not_safe?(speed)(answer)
+if speed < 40 ? answer==true : answer==false
+elsif speed > 60 ? answer==true : answer==false
+elsif speed > 40 && speed < 60 ? answer==false : answer==true
 end
 end
