dea288f494e3ba922f10cd78f52808506f1c9c2e
Alan Young <alanryoung01@gmail.com>
2020-09-10 20:46:28 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,4 +1,4 @@
-require pry
+
 def unsafe?(speed)
 if speed < 40
   return true
@@ -14,6 +14,5 @@ def not_safe?(speed)
 if speed < 40 ? return true : return false
 elsif speed > 60 ? return true : return false
 elsif speed > 40 && speed < 60 ? return false : return true
-  binding.pry
 end
 end
